03084c4eb9a7c4df4abd3cf922c85f4c4406d095
Margo <margo@margonline.co.uk>
2014-01-05 17:43:21 +0000

    .save method to save records to the server

--- public/js/services.js
+++ public/js/services.js
@@ -2,6 +2,9 @@ angular.module('airlineServices', ['ngResource'])
 	.factory('Airport', function  ($resource) {
 		return $resource('/airports/:airportCode');
 	})
-  .factory('Flights', function ($resource) {
-    return $resource('flights');
+	.factory('Flights', function  ($resource) {
+		return $resource('/flights');
+	})
+  .factory('Reservations', function  ($resource) {
+    return $resource('/reservations');
   });
